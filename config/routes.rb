7fdcb9b04c673cc2f10785a409920564325dce71
AlfonsoGtz <g.alfonso_gtz@hotmail.com>
2015-09-11 18:04:36 -0600

    index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index,about,'
-
+  get 'home/index'
   get 'home/video'
 
   # The priority is based upon order of creation: first created -> highest priority.
